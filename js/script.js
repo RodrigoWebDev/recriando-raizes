d694b85d9473945f8ba72c97d11bec4eccfe3d39
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2017-12-30 14:36:12 -0200

    Menu lateral

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var dropdown = document.getElementsByClassName("dropdown-a");
 var painel = document.getElementsByClassName("dropdown-painel");
+var learnMore = document.getElementsByClassName("learn-more")[0];
+var close = document.getElementById("close");
+var menuDisplayButton = document.getElementsByClassName("icon-menu")[0];
 
 for (i=0; i < dropdown.length; i++){
     dropdown[i].onmouseover = function() {
@@ -18,3 +21,28 @@ for (i=0; i < dropdown.length; i++){
     }
 }
     
+learnMore.onmouseover = function() {
+    var icon = this.children[0];
+    var span = this.children[1];
+    span.style.color = "#57bb57";
+    icon.style.color = "#57bb57";
+    this.style.borderBottom = "1px solid #57bb57";
+}
+
+learnMore.onmouseout = function() {
+    var icon = this.children[0];
+    var span = this.children[1];
+    span.style.color = "white";
+    icon.style.color = "white";
+    this.style.borderBottom = "1px solid white";
+}
+
+close.onclick = function() {
+    var menu = document.getElementsByClassName("menu")[0];
+    menu.style.display = "none";
+}
+
+menuDisplayButton.onclick = function() {
+    var menu = document.getElementsByClassName("menu")[0];
+    menu.style.display = "block";
+}
