e7fa3744e908590d08a768bde933970fbb9f4a47
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2017-12-30 22:47:03 -0200

    Imagem de fundo e Painel

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var learnMore = document.getElementsByClassName("learn-more")[0];
 var close = document.getElementById("close");
 var menuDisplayButton = document.getElementsByClassName("icon-menu")[0];
 
+
 for (i=0; i < dropdown.length; i++){
     dropdown[i].onmouseover = function() {
         var drop = this.nextElementSibling;
@@ -45,4 +46,42 @@ close.onclick = function() {
 menuDisplayButton.onclick = function() {
     var menu = document.getElementsByClassName("menu")[0];
     menu.style.display = "block";
+}
+
+var painelButton = document.getElementsByClassName("painel-button");
+var painel = document.getElementsByClassName("painel-inner");
+
+window.onload = function() {
+    painel[0].style.display = "block";
+    painelButton[0].classList.add("painel-button-active");
+    
+}
+
+painelButton[0].onclick = function() {
+    painelButton[0].classList.add("painel-button-active");
+    painelButton[1].classList.remove("painel-button-active");
+    painelButton[2].classList.remove("painel-button-active");
+    painel[0].style.display = "block";
+    painel[1].style.display = "none";
+    painel[2].style.display = "none";
+    
+    
+}
+
+painelButton[1].onclick = function() {
+    painelButton[0].classList.remove("painel-button-active");
+    painelButton[1].classList.add("painel-button-active");
+    painelButton[2].classList.remove("painel-button-active");
+    painel[0].style.display = "none";
+    painel[1].style.display = "block";
+    painel[2].style.display = "none";
+}
+
+painelButton[2].onclick = function() {
+    painelButton[0].classList.remove("painel-button-active");
+    painelButton[1].classList.remove("painel-button-active");
+    painelButton[2].classList.add("painel-button-active");
+    painel[0].style.display = "none";
+    painel[1].style.display = "none";
+    painel[2].style.display = "block";
 }
