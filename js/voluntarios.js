56c763010d8a79f4927ac0c2944eec669fe5756e
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-01-10 00:13:16 -0200

    Adicionado Painel em EQUIPE
    Adicionei um painel para mudança de Divs, ainda incompleto...

--- js/voluntarios.js
+++ js/voluntarios.js
@@ -1,3 +1,4 @@
+//Abre o menu lateral esquerdo quando clicar em algum card de perfil
 let voluntario = document.getElementsByClassName("voluntario");
 let menuLeft = document.getElementsByClassName("menu-left");
 let closeLeftMenu = document.getElementsByClassName("close-left-menu")[0];
@@ -28,4 +29,45 @@ for (i = 0; i < voluntario.length; i++) {
 closeLeftMenu.onclick = function() {
     this.parentElement.parentElement.style.display = "none";
     console.log("click");
+}
+
+//Troca a div ao clicar na TAB
+
+let tab = document.getElementsByClassName("tab");
+let voluntarios1 = document.getElementsByClassName("voluntarios")[0];
+let voluntarios2 = document.getElementsByClassName("voluntarios")[1];
+let voluntarios3 = document.getElementsByClassName("voluntarios")[2];
+voluntarios2.style.display = "none";
+voluntarios3.style.display = "none";
+
+
+
+tab[0].onclick = function() {
+    tab[0].classList.remove("active");
+    tab[1].classList.remove("active");
+    tab[2].classList.remove("active");
+    this.classList.add("active");
+    voluntarios1.style.display = "block";
+    voluntarios2.style.display = "none";
+    voluntarios3.style.display = "none";
+}
+
+tab[1].onclick = function() {
+    tab[0].classList.remove("active");
+    tab[1].classList.remove("active");
+    tab[2].classList.remove("active");
+    this.classList.add("active");
+    voluntarios1.style.display = "none";
+    voluntarios2.style.display = "block";
+    voluntarios3.style.display = "none";
+}
+
+tab[2].onclick = function() {
+    tab[0].classList.remove("active");
+    tab[1].classList.remove("active");
+    tab[2].classList.remove("active");
+    this.classList.add("active");
+    voluntarios1.style.display = "none";
+    voluntarios2.style.display = "none";
+    voluntarios3.style.display = "block";
 }
