bf40808d1b84cd6b7f5ffde217b509a04ef991a4
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-01-27 13:27:27 -0200

    Adicionado Favicon

--- head.php
+++ head.php
@@ -6,3 +6,4 @@
 <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 <link href="css/landing-page.css" rel="stylesheet">
 <link href="css/animate.css" rel="stylesheet"> 
+<link rel="icon" href="img/fav-icon.png">
